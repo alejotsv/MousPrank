cb811c6052ec6d7141377e41ccabb6934f81dcb2
Alejandro Salas <asalas.wd@outlook.com>
2022-11-22 16:20:32 -0500

    Create constructor

--- src/com/java21days/MousePrank.java
+++ src/com/java21days/MousePrank.java
@@ -1,13 +1,21 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MousePrank extends JFrame implements ActionListener {
     MousePrank() {
         super("Message");
+        setSize(420, 220);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        BorderLayout border = new BorderLayout();
+        setLayout(border);
+        JLabel msg = new JLabel("Click 'OK' to close this program");
+        add(BorderLayout.NORTH, msg);
 
+        setVisible(true);
     }
 
     @Override
