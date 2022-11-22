8324513ab4fe62362efa0f506d5039c0708b0e00
Alejandro Salas <asalas.wd@outlook.com>
2022-11-22 16:18:24 -0500

    Initial commit

---
+++ src/com/java21days/MousePrank.java
@@ -0,0 +1,17 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MousePrank extends JFrame implements ActionListener {
+    MousePrank() {
+        super("Message");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
