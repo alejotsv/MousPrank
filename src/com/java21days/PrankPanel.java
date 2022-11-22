1dfc11c84e1780247e80d05be071aea71d4d54f9
Alejandro Salas <asalas.wd@outlook.com>
2022-11-22 16:21:28 -0500

    Create PrankPanel class

---
+++ src/com/java21days/PrankPanel.java
@@ -0,0 +1,19 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class PrankPanel extends JPanel implements MouseMotionListener {
+
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+
+    }
+}
